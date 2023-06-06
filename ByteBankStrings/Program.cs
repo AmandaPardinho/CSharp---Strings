a6d587c6bd46cc489e6735bd3536beb64ea3a638
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-06 16:39:07 -0300

    Correções e inclusão de novos métodos

--- ByteBankStrings/Program.cs
+++ ByteBankStrings/Program.cs
@@ -5,3 +5,38 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using ByteBankStrings.SistemaAgencia;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textoVazio = "";
+            string textoNulo = null;
+            string textoQualquer = "kjhfsdjhgsdfjksdf";
+
+            Console.WriteLine(string.IsNullOrEmpty(textoVazio));
+            Console.WriteLine();
+            Console.WriteLine(string.IsNullOrEmpty(textoNulo));
+            Console.WriteLine();
+            Console.WriteLine(string.IsNullOrEmpty(textoQualquer));
+            Console.WriteLine();
+            Console.ReadLine();
+
+            ExtratorValorArgumentosURL extrator = new ExtratorValorArgumentosURL("");
+
+            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+
+            int indiceInterrogacao = url.IndexOf('?');
+            Console.WriteLine(indiceInterrogacao);
+            Console.WriteLine(url);
+
+            string argumentos = url.Substring(indiceInterrogacao + 1);
+            Console.WriteLine(argumentos);
+
+            Console.ReadLine();
+        }
+    }
+}
