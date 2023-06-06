9e0eaaa3a2c28060b054db3475713d9ac7567d27
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-06 18:21:22 -0300

    Métodos Length e IndexOf com strings

--- ByteBankStrings/Program.cs
+++ ByteBankStrings/Program.cs
@@ -13,6 +13,7 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            //teste do método IsNullOrEmpty
             string textoVazio = "";
             string textoNulo = null;
             string textoQualquer = "kjhfsdjhgsdfjksdf";
@@ -23,18 +24,46 @@ namespace ByteBank.SistemaAgencia
             Console.WriteLine();
             Console.WriteLine(string.IsNullOrEmpty(textoQualquer));
             Console.WriteLine();
-            Console.ReadLine();
 
-            ExtratorValorArgumentosURL extrator = new ExtratorValorArgumentosURL("");
+            ExtratorValorArgumentosURL extrator = new ExtratorValorArgumentosURL("oi");
 
+            //teste do método IndexOf - char
             string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
 
             int indiceInterrogacao = url.IndexOf('?');
             Console.WriteLine(indiceInterrogacao);
             Console.WriteLine(url);
+            Console.WriteLine();
 
             string argumentos = url.Substring(indiceInterrogacao + 1);
             Console.WriteLine(argumentos);
+            Console.WriteLine();
+
+            //teste do método IndexOf - string
+            string palavra = "moedaDestino=real";
+            
+            int indice = palavra.IndexOf("real");
+
+            Console.WriteLine(indice);
+            Console.WriteLine();
+            Console.WriteLine(palavra.Substring(indice));
+
+            //teste método Lenght
+            string palavras = "moedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino";
+            
+            int index = palavras.IndexOf(nomeArgumento);
+            Console.WriteLine(index);
+            Console.WriteLine();
+
+            Console.WriteLine($"Tamanho da string nomeArgumento: {nomeArgumento.Length}");
+            Console.WriteLine();
+
+            Console.WriteLine(palavras);
+            Console.WriteLine();
+            Console.WriteLine(palavras.Substring(index));
+            Console.WriteLine();
+            Console.WriteLine(palavras.Substring(index + nomeArgumento.Length + 1));
 
             Console.ReadLine();
         }
